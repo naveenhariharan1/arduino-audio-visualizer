b4dafb627a6cd49da966aa6a427f5a01526a7120
naveen <naveenhariharan.h@gmail.com>
2026-07-18 15:23:44 +0530

    added button debounce and changed the limit of displayheight as 63

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,13 @@ int displayheight=0;
 int currentbuttonstate=HIGH;
 int previousbuttonstate=HIGH;
 int mode=0;
+unsigned long lastDebounceTime=0;
+unsigned long debounceDelay=40;
 
 float buttonstate(){
+  unsigned long currentTime = millis();
+  if(currentTime-lastDebounceTime>debounceDelay) {
+    lastDebounceTime=currentTime;
   currentbuttonstate=digitalRead(buttonPin);
   if (currentbuttonstate==LOW && previousbuttonstate==HIGH){
     mode++;
@@ -19,17 +24,18 @@ float buttonstate(){
     }}
   if (mode==0){
   digitalWrite(13,HIGH);
-  }
+  } 
   else{
     digitalWrite(13,LOW);
   }
     previousbuttonstate=currentbuttonstate;
-    return mode*0.25;} 
+    return mode*0.25;}
+   }
     
 int audio_and_displayheight(float coef){
   int peak=0;
   int sum=0;
-  for(int r=0;r<60;r++){// peak and avg over 60 interval
+  for(int r=0;r<45;r++){// peak and avg over 60 interval
     int ar=analogRead(A0);
     int currentpeak=abs(ar-512);
     sum+=currentpeak;
@@ -37,13 +43,17 @@ int audio_and_displayheight(float coef){
       peak=currentpeak;
     }
   }
-  int avg=sum/60;
+  int avg=sum/45;
   currentheight=((coef*peak)+(1-coef)*(avg))/8;
   if( currentheight<previousheight){
    displayheight=previousheight-(previousheight-currentheight)/5;
    }
-  else if (previousheight<currentheight){
-    displayheight=min(previousheight+(currentheight-previousheight)/3,64);
+  else if (previousheight<currentheight){   
+    if (currentheight-previousheight>3){
+    displayheight=min(previousheight+(currentheight-previousheight)/3,63);}
+    else{
+      displayheight=currentheight;
+    }
   }
   else {
    displayheight=currentheight;
@@ -53,10 +63,10 @@ int audio_and_displayheight(float coef){
 }
 
 void display(int displayheight){
-  for (int i=0;i<displayheight;i++){
+  for (int i=0;i<=displayheight;i++){
     leds[i]=CHSV(95-i*1.5,255,255);
   }
-  for (int i=displayheight;i<NUMofLEDS;i++){
+  for (int i=displayheight+1;i<NUMofLEDS;i++){
     leds[i]=CRGB::Black;
   }
   FastLED.show();
